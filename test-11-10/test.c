aa3ca8884390d62aa3a22503d1bd94536ceffa9c
ljh <ljh@LAPTOP-JPKIM66F>
2022-11-11 22:55:36 +0800

    添加项目文件。

---
+++ test-11-10/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main()
+{
+	printf("hehe\n");
+	return 0;
+}
+//int main()//主函数-程序的入口-main函数有且只有一个
+//{
+//	//int是整型的意思
+//	//
+//	printf("hello world\n");//\n表示换行
+//	return 0;//返回0
+//}
